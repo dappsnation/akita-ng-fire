b6c042d672a02cdf41bc5e7104e2afc3910bd2fe
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-09-20 16:14:27 +0200

    clean example

--- src/app/dynamic-store/marketplace.module.ts
+++ src/app/dynamic-store/marketplace.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+
+import { MatListModule } from '@angular/material/list';
+
 import { MarketplaceHomeComponent } from './marketplace-home/marketplace-home.component';
 import { MarketplaceCarouselComponent } from './marketplace-carousel/marketplace-carousel.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatListModule,
     RouterModule.forChild([{ path: '', component: MarketplaceHomeComponent }])
   ],
   declarations: [MarketplaceHomeComponent, MarketplaceCarouselComponent]
