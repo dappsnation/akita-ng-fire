5b82cfd90439740d5c8e5a6f84e7e169930a48f2
Grandschtroumpf <francois.guezengar@hotmail.fr>
2019-09-29 08:41:06 +0200

    Add organization example

--- src/app/auth/+state/auth.service.ts
+++ src/app/auth/+state/auth.service.ts
@@ -1,10 +1,13 @@
 import { Injectable } from '@angular/core';
 import { FireAuthService, CollectionConfig } from 'akita-ng-fire';
 import { AuthStore, AuthState } from './auth.store';
+import { createProfile } from './auth.model';
 
 @Injectable({ providedIn: 'root' })
 @CollectionConfig({ path: 'users' })
 export class AuthService extends FireAuthService<AuthState> {
+
+  fromFirestore = createProfile;
 
   constructor(store: AuthStore) {
     super(store);
--- src/app/sync-many-ids/organization.module.ts
+++ src/app/sync-many-ids/organization.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { OrganizationCreateComponent } from './organization-create/organization-create.component';
-import { OrganizationListComponent } from './organization-list/organization-list.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MatListModule } from '@angular/material/list';
-import { OrganizationListGuard } from './organization.guard';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSelectModule } from '@angular/material/select';
+
+import { OrganizationListGuard, ActiveOrganizationGuard } from './organization.guard';
+import { OrganizationCreateComponent } from './organization-create/organization-create.component';
+import { OrganizationListComponent } from './organization-list/organization-list.component';
+import { OrganizationViewComponent } from './organization-view/organization-view.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ReactiveFormsModule,
     MatListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatSelectModule,
     RouterModule.forChild([
       { path: '', redirectTo: 'list', pathMatch: 'full' },
       {
@@ -18,9 +32,16 @@ import { OrganizationListGuard } from './organization.guard';
         component: OrganizationListComponent,
         canActivate: [OrganizationListGuard],
         canDeactivate: [OrganizationListGuard],
-      }
+      },
+      { path: 'create', component: OrganizationCreateComponent },
+      {
+        path: 'view/:orgId',
+        component: OrganizationViewComponent,
+        canActivate: [ActiveOrganizationGuard],
+        canDeactivate: [ActiveOrganizationGuard],
+      },
     ])
   ],
-  declarations: [OrganizationCreateComponent, OrganizationListComponent]
+  declarations: [OrganizationCreateComponent, OrganizationListComponent, OrganizationViewComponent]
 })
 export class OrganizationModule {}
