754289c1a17b86245fcbce371acfdd64940be9ef
Alexis Merceron <contact@alexismerceron.com>
2019-10-09 20:19:26 +0200

    fix imports firebase

--- projects/akita-ng-fire/src/lib/collection/collection.service.ts
+++ projects/akita-ng-fire/src/lib/collection/collection.service.ts
@@ -15,7 +15,8 @@ import {
   getEntityType,
   getIDType,
 } from '@datorama/akita';
-import { firestore } from 'firebase';
+import * as firebase from 'firebase/app';
+import 'firebase/firestore';
 import { CollectionOptions } from './collection.config';
 import { getIdAndPath } from '../utils/id-or-path';
 import {
@@ -373,7 +374,7 @@ export class CollectionService<S extends EntityState<any, string>>  {
     const ids = await Promise.all(operations);
     // If there is no atomic write provided
     if (!options.write) {
-      return (write as firestore.WriteBatch).commit();
+      return (write as firebase.firestore.WriteBatch).commit();
     }
     return Array.isArray(documents) ? ids : ids[0];
   }
@@ -396,7 +397,7 @@ export class CollectionService<S extends EntityState<any, string>>  {
     await Promise.all(operations);
     // If there is no atomic write provided
     if (!options.write) {
-      return (write as firestore.WriteBatch).commit();
+      return (write as firebase.firestore.WriteBatch).commit();
     }
   }
 
@@ -414,7 +415,7 @@ export class CollectionService<S extends EntityState<any, string>>  {
     idsOrEntity: Partial<getEntityType<S>> | string | string[],
     stateFnOrWrite?: UpdateStateCallback<getEntityType<S>> | Partial<getEntityType<S>> | WriteOptions,
     options: WriteOptions = {}
-  ): Promise<void | firestore.Transaction[]> {
+  ): Promise<void | firebase.firestore.Transaction[]> {
 
     let ids: string[] = [];
     let newStateOrFn = stateFnOrWrite as UpdateStateCallback<getEntityType<S>> | Partial<getEntityType<S>>;
@@ -468,7 +469,7 @@ export class CollectionService<S extends EntityState<any, string>>  {
       await Promise.all(operations);
       // If there is no atomic write provided
       if (!options.write) {
-        return (write as firestore.WriteBatch).commit();
+        return (write as firebase.firestore.WriteBatch).commit();
       }
     }
   }
