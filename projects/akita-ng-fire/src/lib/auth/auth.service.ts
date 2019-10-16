41be4d83fda5f4296ccf58fa27a468425f0c4cea
Alexis Merceron <contact@alexismerceron.com>
2019-10-16 19:36:20 +0200

    fix firebase imports

--- projects/akita-ng-fire/src/lib/auth/auth.service.ts
+++ projects/akita-ng-fire/src/lib/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { inject } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
-import * as firebase from 'firebase/app';
+import { auth, User } from 'firebase/app';
 import 'firebase/auth';
 import { switchMap, tap } from 'rxjs/operators';
 import { Observable, of, combineLatest } from 'rxjs';
@@ -19,12 +19,12 @@ function isFireAuthProvider(provider: string): provider is FireProvider {
 
 function getAuthProvider(provider: FireProvider) {
   switch (provider) {
-    case 'email': return new firebase.auth.EmailAuthProvider();
-    case 'facebook': return new firebase.auth.FacebookAuthProvider();
-    case 'github': return new firebase.auth.GithubAuthProvider();
-    case 'google': return new firebase.auth.GoogleAuthProvider();
-    case 'microsoft': return new firebase.auth.OAuthProvider('microsoft.com');
-    case 'twitter': return new firebase.auth.TwitterAuthProvider();
+    case 'email': return new auth.EmailAuthProvider();
+    case 'facebook': return new auth.FacebookAuthProvider();
+    case 'github': return new auth.GithubAuthProvider();
+    case 'google': return new auth.GoogleAuthProvider();
+    case 'microsoft': return new auth.OAuthProvider('microsoft.com');
+    case 'twitter': return new auth.TwitterAuthProvider();
   }
 }
 
@@ -45,17 +45,17 @@ export class FireAuthService<S extends FireAuthState> {
   }
 
   /** Can be overrided */
-  protected selectProfile(user: firebase.User): Observable<S['profile']> {
+  protected selectProfile(user: User): Observable<S['profile']> {
     return this.collection.doc<S['profile']>(user.uid).valueChanges();
   }
 
   /** Can be overrided */
-  protected selectRoles(user: firebase.User): Promise<S['roles']> | Observable<S['roles']> {
+  protected selectRoles(user: User): Promise<S['roles']> | Observable<S['roles']> {
     return user.getIdTokenResult().then(({ claims }) => claims as any);
   }
 
   /** Should be override */
-  protected createProfile(user: firebase.User): Promise<Partial<S['profile']>> | Partial<S['profile']> {
+  protected createProfile(user: User): Promise<Partial<S['profile']>> | Partial<S['profile']> {
     return {
       photoURL: user.photoURL,
       displayName: user.displayName,
@@ -117,7 +117,7 @@ export class FireAuthService<S extends FireAuthState> {
   }
 
   /** Create a user based on email and password */
-  async signup(email: string, password: string): Promise<firebase.auth.UserCredential> {
+  async signup(email: string, password: string): Promise<auth.UserCredential> {
     const cred = await this.fireAuth.auth.createUserWithEmailAndPassword(email, password);
     const profile = await this.createProfile(cred.user);
     const batch = this.db.firestore.batch();
@@ -130,13 +130,13 @@ export class FireAuthService<S extends FireAuthState> {
   }
 
   // tslint:disable-next-line: unified-signatures
-  signin(email: string, password: string): Promise<firebase.auth.UserCredential>;
-  signin(provider?: FireProvider): Promise<firebase.auth.UserCredential>;
-  signin(token: string): Promise<firebase.auth.UserCredential>;
-  async signin(provider?: FireProvider | string, password?: string): Promise<firebase.auth.UserCredential> {
+  signin(email: string, password: string): Promise<auth.UserCredential>;
+  signin(provider?: FireProvider): Promise<auth.UserCredential>;
+  signin(token: string): Promise<auth.UserCredential>;
+  async signin(provider?: FireProvider | string, password?: string): Promise<auth.UserCredential> {
     this.store.setLoading(true);
     try {
-      let cred: firebase.auth.UserCredential;
+      let cred: auth.UserCredential;
       if (!provider) {
         cred = await this.fireAuth.auth.signInAnonymously();
       } else if (password) {
