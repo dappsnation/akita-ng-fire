158c925eb5f2f5365faa7cccafcea3ff667f8de3
grandschtroumpf <francois.guezengar@hotmail.fr>
2020-03-20 12:22:28 +0100

    Update to @angular 9

--- src/app/dynamic-store/+state/marketplace.query.ts
+++ src/app/dynamic-store/+state/marketplace.query.ts
@@ -1,6 +1,8 @@
 import { QueryEntity } from '@datorama/akita';
 import { MarketplaceStore, MarketplaceState } from './marketplace.store';
+import { Injectable } from '@angular/core';
 
+@Injectable()
 export class MarketplaceQuery extends QueryEntity<MarketplaceState> {
 
   constructor(protected store: MarketplaceStore) {
